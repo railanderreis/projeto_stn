6a1ef72058e88ccc28cea3fc1c15912bc5485e38
railanderreis <railander_ios@hotmail.com>
2021-04-02 12:39:11 -0300

    adicionar scripts 01

---
+++ scripts/bacen/s3_upload.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+from datetime import datetime
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+client = boto3.client('s3')
+
+
+def upload_to_s3(bucket, schema, table, partition, json_data):
+
+    now = datetime.now()
+    now_string = now.strftime("%Y-%m-%d")
+    key = f'{schema}/{table}/execution_date={partition}/{schema}_{table}_{now_string}.json'
+
+    logging.info(f'Uploading file to S3 with key: {key}')
+
+    bin_data = json.dumps(json_data).encode('utf-8')
+    return client.put_object(Body=bin_data, Bucket=bucket, Key=key)
---
+++ scripts/pgfn/pgfn_ativo.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import zipfile
+import urllib.request
+
+
+def get_data():
+    url = 'http://dadosabertos.pgfn.gov.br/Dados_abertos_Nao_Previdenciario.zip'
+    
